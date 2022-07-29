717d5f3db2144042d235ee8593685f088ee503f3
irinkwink <irina-ns@yandex.ru>
2022-07-29 20:12:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsOpen = [];
+  let bracketsClose = [];
+  let openStr = [];
+  let isOpenBracket = {};
+
+  bracketsConfig.forEach(item => {
+    bracketsOpen.push(item[0]);
+    bracketsClose.push(item[1]);
+    if (item[0] === item[1]) {
+      isOpenBracket[item[0]] = true;
+    }
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    if (bracketsClose.indexOf(str[i]) !== -1) {
+      if (bracketsClose.indexOf(str[i]) === bracketsOpen.indexOf(str[i]) && isOpenBracket[str[i]]) {
+        openStr.push(str[i]);
+        isOpenBracket[str[i]] = false;
+      } else {
+        if (bracketsClose.indexOf(str[i]) === bracketsOpen.indexOf(str[i])) {
+          isOpenBracket[str[i]] = true;
+        }
+        if (bracketsClose.indexOf(str[i]) !== bracketsOpen.indexOf(openStr.pop())) {
+          return false;
+        }
+      }
+    } else {
+      openStr.push(str[i]);
+    }
+  }
+
+  return !openStr.length;
 }
